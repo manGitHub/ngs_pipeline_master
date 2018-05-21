d25c2dcafc577510d7ac7e264ac7689dadf782cb
manGitHub <16341141+manGitHub@users.noreply.github.com>
2018-05-21 11:30:47 -0400

    Remove Defuse

--- scripts/ActionableFusion.v1.pl
+++ scripts/ActionableFusion.v1.pl
@@ -10,36 +10,37 @@ use List::Util qw(first);
 #
 ###########################################################
 my $library        = $ARGV[0];
-my $defuse         = $ARGV[1];
-my $tophat         = $ARGV[2];
-my $fusioncatcher  = $ARGV[3];
-my $star_fusion	   = $ARGV[4];
-my $destination    = $ARGV[5];
+## my $defuse         = $ARGV[X];  NOT used 5/11/2018
+my $tophat         = $ARGV[1];
+my $fusioncatcher  = $ARGV[2];
+my $star_fusion	   = $ARGV[3];
+my $destination    = $ARGV[4];
 print "#LeftGene\tRightGene\tChr_Left\tPosition\tChr_Right\tPosition\tSample\tTool\tSpanReadCount\n";
 ###########################################################
+# Comment Defuse file parsing 5/11/2018
 ###########################################################
-unless (open(DEFUSE, "$defuse")){
-	print STDERR "Can not open the file $defuse\n";
-	exit;
-}
-unless (open(DEFUSE_OUT, ">$destination/$library.defuse.filtered.txt")){
-	print STDERR "Can not open the file $destination/$library.defuse.filtered.txt\n";
-	die $!;
-}
-my $idx;
-while(<DEFUSE>){
-	chomp;
-	my @local =split("\t", $_);
-	if($_ =~ /^cluster_id/){
-		print DEFUSE_OUT "$_\n"; 
-		$idx = first { $local[$_] eq 'span_count' } 0..$#local;
-		next;
-	}	
-	print DEFUSE_OUT "$_\n";
-	print "$local[30]\t$local[31]\tchr$local[24]\t$local[37]\tchr$local[25]\t$local[38]\t$library\tDefuse\t$local[$idx]\n";
-}
-close DEFUSE;
-close DEFUSE_OUT;
+#unless (open(DEFUSE, "$defuse")){
+#	print STDERR "Can not open the file $defuse\n";
+#	exit;
+#}
+#unless (open(DEFUSE_OUT, ">$destination/$library.defuse.filtered.txt")){
+#	print STDERR "Can not open the file $destination/$library.defuse.filtered.txt\n";
+#	die $!;
+#}
+#my $idx;
+#while(<DEFUSE>){
+#	chomp;
+#	my @local =split("\t", $_);
+#	if($_ =~ /^cluster_id/){
+#		print DEFUSE_OUT "$_\n"; 
+#		$idx = first { $local[$_] eq 'span_count' } 0..$#local;
+#		next;
+#	}	
+#	print DEFUSE_OUT "$_\n";
+#	print "$local[30]\t$local[31]\tchr$local[24]\t$local[37]\tchr$local[25]\t$local[38]\t$library\tDefuse\t$local[$idx]\n";
+#}
+#close DEFUSE;
+#close DEFUSE_OUT;
 ###########################################################
 unless (open(TOP, "$tophat")){
         print STDERR "Can not open the file $tophat\n";
